e1fde9c711d7aba915726ff6ddd569cfc3e00564
whoshwj <whoshoo25@gmail.com>
2021-12-20 17:48:57 +0900

    users 테이블 제약조건 추가

---
+++ github/DB/table_sql/constraint.sql
@@ -0,0 +1,6 @@
+ALTER TABLE users MODIFY user_role DEFAULT 1;
+ALTER TABLE users MODIFY user_privacy DEFAULT 1;
+ALTER TABLE users MODIFY message_alarm_check DEFAULT 1;
+ALTER TABLE users MODIFY updates_alarm_check DEFAULT 1;
+ALTER TABLE users MODIFY follow_alarm_check DEFAULT 1;
+ALTER TABLE users MODIFY marketing_alarm_check DEFAULT 1;
