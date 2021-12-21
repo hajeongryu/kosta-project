514c299267326a7c1ee872e46d2f9acea4a25f48
dongjea2 <dewawd@gmail.com>
2021-12-21 21:11:35 +0900

    메인페이지+ 카테고리전체 .sql 수정

--- github/DB/페이지별쿼리문/메인 페이지.sql
+++ github/DB/페이지별쿼리문/메인 페이지.sql
@@ -1,48 +1,59 @@
---1. 주목할만한 프로젝트(8개)
-SELECT project_image "프로젝트 이미지"
-        ,category_no "카테고리 번호"
+--아우터조인으로 카테고리 이름 추가해야함
+
+--1. 주목할만한 프로젝트(8개) = 달성률 100%이상 
+SELECT  p.project_no"프로젝트 번호"
+        ,category_name "카테고리 이름"
         , user_name "프로젝트 작성자 이름"
         , long_title "프로젝트 제목"
         , target_price "목표금액"
-        , get_price "모인금액" --목표금액/모인금액
+        , sum_price "모인금액" --목표금액/모인금액
         , end_date "종료일" --sysdate에서 enddate빼기
-        
+        , support_cnt"후원자 수"
+        , project_image "프로젝트 이미지"
 FROM project p
     JOIN project_change c
         ON p.project_no =c.project_no
     JOIN users u
         ON p.user_no = u.user_no
-    JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
-        ON p.project_no = orders.project_no
+    JOIN category cate
+        ON p.category_no = cate.category_no
 
--- 달성률 100% 이상 + 마감임박 인것들
-WHERE (c.get_price / p.target_price) >1.00
-    AND p.end_date<sysdate  --1월2일   < 1월3일
+-- 달성률 100% 이상
+WHERE (c.sum_price / p.target_price) >1.00
+    AND sysdate < p.end_date --진행중 '승인' 필요없음 이미 모금됬기떄문에
     AND ROWNUM <=8
 ORDER BY p.end_date ASC;
 
 
 
 
---2.인기프로젝트(12개) 
-SELECT project_image "프로젝트 이미지"
-        ,category_no "카테고리 번호"
+--2.인기프로젝트(12개) =좋아요 수
+SELECT  p.project_no"프로젝트 번호"
+        ,category_name "카테고리 이름"
         , user_name "프로젝트 작성자 이름"
         , long_title "프로젝트 제목"
         , target_price "목표금액"
-        , get_price "모인금액" --목표금액/모인금액
+        , sum_price "모인금액" --목표금액/모인금액
         , end_date "종료일" --sysdate에서 enddate빼기
-        
+        , support_cnt"후원자 수"
+        , project_image "프로젝트 이미지"
 FROM project p
     JOIN project_change c
         ON p.project_no =c.project_no
     JOIN users u
         ON p.user_no = u.user_no
-    JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
-        ON p.project_no = orders.project_no
+    JOIN category cate
+        ON p.category_no = cate.category_no
+
         
-    --진행중이면서 인기순으로
-WHERE c.project_status = '승인' and 0>(p.end_date- sysdate)
+WHERE c.project_status IN ('승인')
+        --시작중+마감일 안지난거 = 진행중
+    -- 시작 된거 (시작일)1월5일 <(오늘)1월6일   ||         (1월6일) < 현재(1월5일) (x)
+    AND sysdate > P.start_date
+    
+     -- 마감일 안 지난거 (o) 0 > -1(음수) = 오늘(1/1)- 마감일(1/2)  || (x) 0 > 4(양수) = 오늘(1/5) -마감일(1/1)  
+    AND 0>= (sysdate -p.end_date ) --0이면 날짜같음
+    
     AND ROWNUM <=12
 
 ORDER BY c.project_like_cnt DESC;
@@ -53,73 +64,100 @@ ORDER BY c.project_like_cnt DESC;
 --3.최근 본 프로젝트 (미구현)
 
 
---4.마감임박! 프로젝트(16개)
-SELECT project_image "프로젝트 이미지"
-        ,category_no "카테고리 번호"
+--4.마감임박! 프로젝트(16개) = 진행중 + 곧 마감
+SELECT  p.project_no"프로젝트 번호"
+        ,category_name "카테고리 이름"
         , user_name "프로젝트 작성자 이름"
         , long_title "프로젝트 제목"
         , target_price "목표금액"
-        , get_price "모인금액" --목표금액/모인금액
+        , sum_price "모인금액" --목표금액/모인금액
         , end_date "종료일" --sysdate에서 enddate빼기
-        
+        , support_cnt"후원자 수"
+        , project_image "프로젝트 이미지"
 FROM project p
     JOIN project_change c
         ON p.project_no =c.project_no
     JOIN users u
         ON p.user_no = u.user_no
-    JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
-        ON p.project_no = orders.project_no
+    JOIN category cate
+        ON p.category_no = cate.category_no
         
-    --진행중이면서, 마감임박
-WHERE c.project_status = '승인' and 0>(p.end_date- sysdate)
-    AND ROWNUM <=16
-    AND p.end_date<sysdate  --1월2일   < 1월3일
+WHERE c.project_status IN ('승인')
+        -- 진행중 =시작됨+마감일 안지난거
+    -- 시작 된거 (시작일)1월5일 <(오늘)1월6일   ||         (1월6일) < 현재(1월5일) (x)
+    AND sysdate > P.start_date
+     -- 마감일 안 지난거 (o) 0 > -1(음수) = 오늘(1/1)- 마감일(1/2)  || (x) 0 > 4(양수) = 오늘(1/5) -마감일(1/1)  
+    AND -10 >= (sysdate -p.end_date ) --10일전       -10=오늘(1/10) -종료일(1/20)(종료 10일전) 
+                                      --            -3 =오늘(1/17) - 종료일(1/20) (종료3일전)
+    
+    
 ORDER BY p.end_date ASC;
 
 
 
---5.공개예정 프로젝트(16개)
-SELECT project_image "프로젝트 이미지"
-        ,category_no "카테고리 번호"
+--5.공개예정 프로젝트(16개)  0k
+SELECT  p.project_no"프로젝트 번호"
+        ,category_name "카테고리 이름"
         , user_name "프로젝트 작성자 이름"
         , long_title "프로젝트 제목"
         , target_price "목표금액"
-        , get_price "모인금액" --목표금액/모인금액
+        , sum_price "모인금액" --목표금액/모인금액
         , end_date "종료일" --sysdate에서 enddate빼기
-        
+        , support_cnt"후원자 수"
+        , project_image "프로젝트 이미지"
 FROM project p
     JOIN project_change c
         ON p.project_no =c.project_no
     JOIN users u
         ON p.user_no = u.user_no
-    JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
-        ON p.project_no = orders.project_no
+    JOIN category cate
+        ON p.category_no = cate.category_no
+
         
-    --공개 예정
-WHERE c.project_status = '승인' AND p.end_date > sysdate
+    --공개 예정 = 시작안된거 + 승인된거
+WHERE c.project_status = '승인' 
+    --  시작안된거    오늘(1/1) < 시작일(1/20)
+    AND  sysdate  < p.start_date 
     AND ROWNUM <=16
     
 ORDER BY p.start_date DESC;
 
 
 
---6.신규프로젝트
+--6.신규프로젝트(ok)
 
-SELECT project_image "프로젝트 이미지"
-        ,category_no "카테고리 번호"
+SELECT  p.project_no"프로젝트 번호"
+        ,category_name "카테고리 이름"
         , user_name "프로젝트 작성자 이름"
         , long_title "프로젝트 제목"
         , target_price "목표금액"
-        , get_price "모인금액" --목표금액/모인금액
+        , sum_price "모인금액" --목표금액/모인금액
         , end_date "종료일" --sysdate에서 enddate빼기
-        
+        , support_cnt"후원자 수"
+        , project_image "프로젝트 이미지"
 FROM project p
     JOIN project_change c
         ON p.project_no =c.project_no
     JOIN users u
         ON p.user_no = u.user_no
-    JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
-        ON p.project_no = orders.project_no
+    JOIN category cate
+        ON p.category_no = cate.category_no
 
-WHERE c.project_status = '승인됨' --일단은 진행중인 프로젝트들이 젤먼저 보임
-ORDER BY p.start_date DESC; --내림차순(최신일이 맨위)      
+WHERE c.project_status IN ('승인')
+        --시작중+마감일 안지난거 = 진행중
+    -- 시작 된거 (시작일)1월5일 <(오늘)1월6일   ||         (1월6일) < 현재(1월5일) (x)
+    AND sysdate > P.start_date
+    
+     -- 마감일 안 지난거 (o) 0 > -1(음수) = 오늘(1/1)- 마감일(1/2)  || (x) 0 > 4(양수) = 오늘(1/5) -마감일(1/1)  
+    AND 0>= (sysdate -p.end_date ) --0이면 날짜같음
+    
+    
+    --프로젝트 시작한지 2주이내  5일지남 = 오늘(1/10) - 시작일(1/5) >    || 25일지남 = (1/30)오늘 - (1/5)시작일
+    AND  14>= (sysdate - p.start_date) --14일지남
+    
+    
+ORDER BY p.start_date DESC; --내림차순(최신일이 맨위)  
+
+
+
+
--- github/DB/페이지별쿼리문/카테고리 전체.sql
+++ github/DB/페이지별쿼리문/카테고리 전체.sql
@@ -1,19 +1,22 @@
-SELECT project_image "프로젝트 이미지"
-        , long_title "프로젝트 제목"
-        ,category_no "카테고리 번호"
+SELECT  p.project_no"프로젝트 번호"
+        ,category_name "카테고리 이름"
         , user_name "프로젝트 작성자 이름"
-        , project_brief "프로젝트 요약"
+        , long_title "프로젝트 제목"
         , target_price "목표금액"
-        , get_price "모인금액" --목표금액/모인금액
+        , sum_price "모인금액" --목표금액/모인금액
         , end_date "종료일" --sysdate에서 enddate빼기
-        
+        , support_cnt"후원자 수"
+        , project_image "프로젝트 이미지"
 FROM project p
     JOIN project_change c
         ON p.project_no =c.project_no
     JOIN users u
         ON p.user_no = u.user_no
-    JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
-        ON p.project_no = orders.project_no
+    JOIN category cate
+        ON p.category_no = cate.category_no
+
+    --JOIN orders  --쓰이지는 않는 경우도 있는데 코드 재활용성을 위해 그냥 넣어둠
+      --  ON p.project_no = orders.project_no;
         
         
         
@@ -36,14 +39,25 @@ FROM project p
 --@리퀘스트값  "ongoing" = onGoing(진행중 프로젝트) /confirm(성사된 프로젝트) /prelaunching(공개예정 프로젝트) / null
 
 
-    --1.onGoing(진행중 프로젝트)--
---AND c.project_status = '승인' and 0>(p.end_date- sysdate);
+    -- 2-1.onGoing(진행중 프로젝트)--
+--AND c.project_status = '승인' 
 
-    --2.confirm(성사된 프로젝트)--
---AND  orders.payment_result = 1; 
+    --시작 된거/    오늘(1/6) > 시작일(1/5)  ||   오늘(1/1)  < 시작일(1/6) (x)
+--AND sysdate > P.start_date 
+    --마감일 안 지난거/    0 > -1 = 오늘(1/1)- 마감일(1/2) <음수>   || (x) 0 > 19  = 오늘(1/20)  -  마감일(1/1) 
+--AND 0>= (sysdate -p.end_date ) 
+    
 
-    --3.prelaunching(공개예정 프로젝트)---
---AND c.project_status = '승인' AND p.end_date > sysdate;
+
+    -- 2-2.confirm(성사된 프로젝트) (마감됨 + 모금액 초과)-- 
+    --마감일 지난거   (o) 0< 9 =오늘(1/10)- 마감일(1/1)      || (x)  0 < -5 =오늘(1/5) - 마감일(1/10)  
+--AND 0< (sysdate - p.end_date)
+--AND c.sum_price > p.target_price;
+
+
+    -- 2-3.prelaunching(공개예정 프로젝트)---
+--AND c.project_status = '승인' 
+--AND sysdate  < p.start_date;  -- 오늘(1/5) < 시작일(1/20)  
 
 
 
@@ -54,7 +68,7 @@ FROM project p
 --@리퀘스트값 editorpick = '1' / null
 
 
-    --1.에디터 추천 프로젝트(1)--
+    --3-1.에디터 추천 프로젝트(1)--
 --AND p.editor_pick = '1';
 
 
@@ -64,15 +78,15 @@ FROM project p
 --4.<달성률>==============================--
 --@리퀘스트값 achieveRate=    (1) 75%이하   (2)75~100%    (3)100% 이상  / null
 
-    --1.  75% 이하 달성률
---AND (c.get_price / p.target_price) <0.75
+    --4-1.  75% 이하 달성률
+--AND (c.sum_price / p.target_price) <0.75
 
-    --2.  75~100% 달성률
---AND (c.get_price / p.target_price) BETWEEN 0.75 AND 1.00
+    --4-2.  75~100% 달성률
+--AND (c.sum_price / p.target_price) BETWEEN 0.75 AND 1.00
 
 
-    --3.  100%이상 달성률
---AND (c.get_price / p.target_price) >1.00
+    --4-3.  100%이상 달성률
+--AND (c.sum_price / p.target_price) >1.00
 
 
 
@@ -81,24 +95,32 @@ FROM project p
 
 
 --5.<정렬>================================--
---@리퀘스트값 editorpick = '1'  /null
+--@리퀘스트값 sort = popular(인기순)	publishedAt(최신순)	pledges(후원순)	amount(금액순) ended(마감 임박순)
 
 
-   --1.인기순
+   --1.popular(인기순) 좋아요순
 --ORDER BY c.project_like_cnt DESC
-    --2.후원순
---ORDER BY c.support_cnt DESC
-    --3.금액순
---ORDER BY c.get_price DESC
 
-    --4.최신순
---  AND c.project_status = '승인됨' --일단은 진행중인 프로젝트들이 젤먼저 보임
+    --2.pledges(후원순)
+--ORDER BY c.support_cnt DESC
+
+    --3.amount(금액순)
+--ORDER BY c.sum_price DESC
+
+    --4.publishedAt(최신순)
+--AND c.project_status = '승인' 
 --ORDER BY p.start_date DESC --내림차순(최신일이 맨위)
 
-    --5.마감임박순
+    --5.ended(마감 임박순)
 --AND p.end_date<sysdate  --1월2일   < 1월3일
 --ORDER BY p.end_date ASC
 
-
 ;
 --END
+
+SELECT p.project_no , start_date, end_date,project_status ,user_name, sum_price
+FROM project p
+JOIN project_change c
+    ON  p.project_no = c.project_no
+JOIN users u
+    ON  u.user_no = p.user_no;
