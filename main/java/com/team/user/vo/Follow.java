59bb0db1af2f001f0c7aaf2aac908ee57f284a81
Jihoon Oh <bs4079.dev@gmail.com>
2021-12-24 01:58:43 +0900

    Follow Vo 수정

--- main/java/com/team/user/vo/Follow.java
+++ main/java/com/team/user/vo/Follow.java
@@ -9,14 +9,16 @@ import java.util.List;
 public class Follow {
 	private int userNo;
 	private List<Users> following;
+	private List<Users> follower;
 	public Follow() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Follow(int userNo, List<Users> following) {
+	public Follow(int userNo, List<Users> following, List<Users> follower) {
 		super();
 		this.userNo = userNo;
 		this.following = following;
+		this.follower = follower;
 	}
 	public int getUserNo() {
 		return userNo;
@@ -27,8 +29,14 @@ public class Follow {
 	public List<Users> getFollowing() {
 		return following;
 	}
-	public void setFollowing(List<Users> following) {
+	public void setFollowering(List<Users> following) {
 		this.following = following;
+	}
+	public List<Users> getFollower() {
+		return follower;
+	}
+	public void setFollower(List<Users> follower) {
+		this.follower = follower;
 	}
 
 	
