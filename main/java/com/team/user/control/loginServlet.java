81c5ce7d7e77520c040e6adbe3cb9f99d772cdad
Jihoon Oh <bs4079.dev@gmail.com>
2021-12-23 09:55:52 +0900

    userDAO findBy 2가지 추가

--- main/java/com/team/user/control/loginServlet.java
+++ main/java/com/team/user/control/loginServlet.java
@@ -1,11 +1,18 @@
 package com.team.user.control;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.team.exception.FindException;
+import com.team.user.service.UserService;
+import com.team.user.vo.Users;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -14,7 +21,27 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String idValue = request.getParameter("id");
 		String pwdValue = request.getParameter("pwd");
-		System.out.println("userId=" + idValue + ", pwd" + pwdValue);
+		System.out.println("userId=" + idValue + ", userPwd" + pwdValue);
+		String resultMsg="";
+		UserService service = UserService.getInstance();
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("loginInfo");
+		
+		String path = "";
+		try {
+			Users u = service.login(idValue, pwdValue);
+			System.out.println("로그인 성공");
+			session.setAttribute("loginInfo", u);
+		} catch (FindException e) {
+			System.out.println(e.getMessage());
+			resultMsg = "로그인 실패";
+			request.setAttribute("status", 0);
+		}
+		
+		request.setAttribute("msg", resultMsg);
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
 	}
 
 }
